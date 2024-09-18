184e5ca94de741c36992dd75b0d2dc4c3346d82e
fabmaiadev <fabxmaia@outlook.com>
2024-09-18 16:30:51 -0300

    fabsmain: testes para a classe de controller de vaga e veiculo

--- src/test/java/com/example/teste/cond_vagas_garagem/controllers/VagaControllerTest.java
+++ src/test/java/com/example/teste/cond_vagas_garagem/controllers/VagaControllerTest.java
@@ -1,24 +1,164 @@
 package com.example.teste.cond_vagas_garagem.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.AfterEach;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.teste.cond_vagas_garagem.dtos.VagaDto;
+import com.example.teste.cond_vagas_garagem.exceptions.ConstraintViolationException;
+import com.example.teste.cond_vagas_garagem.exceptions.NotFoundObjectException;
+import com.example.teste.cond_vagas_garagem.exceptions.NotNullableFieldsException;
+import com.example.teste.cond_vagas_garagem.models.Morador;
+import com.example.teste.cond_vagas_garagem.models.Vaga;
+import com.example.teste.cond_vagas_garagem.services.VagaService;
 
 class VagaControllerTest {
 
+	@Mock private VagaService service;
+	@InjectMocks private VagaController controller;
+	
 	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testCadastrarVagaDeveRetornarHttpStatus201() {
+		
+		VagaDto validVagaDto = new VagaDto("validNumeroVaga", true, 1L, 2L);
+		VagaDto validVagaDtoSaved = new VagaDto("validNumeroVaga", true, 1L, 2L);
+		
+		when(service.saveVaga(validVagaDto)).thenReturn(validVagaDtoSaved);
+		
+		ResponseEntity<VagaDto> response = controller.addVaga(validVagaDto);
+		
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		assertEquals(validVagaDtoSaved, response.getBody());
 	}
+	
+	@Test
+	public void testCadastrarVagaDeveRetornarHttpStatus409() {
+		
+		VagaDto invalidVagaDto = new VagaDto("validNumeroVaga", true, 1L, 2L);
+		
+		doThrow(ConstraintViolationException.class).when(service).saveVaga(invalidVagaDto);
+		
+		assertThrows(ConstraintViolationException.class, () -> controller.addVaga(invalidVagaDto));
+	}
+	
+	@Test
+	public void testCadastrarVagaDeveRetornarHttpStatus406() {
+		
+		VagaDto invalidVagaDto = null;
+		
+		doThrow(NotNullableFieldsException.class).when(service).saveVaga(invalidVagaDto);
+		
+		assertThrows(NotNullableFieldsException.class, () -> controller.addVaga(invalidVagaDto));
+	}
+	
+	@Test
+	public void testFindVagaByIdDeveRetornarHttp200() {
+		
+		Morador validMorador = new Morador("validNomedoMorador", "validApartamento", "validBloco");
+		Long validId = 1L;
+		Vaga validVaga = new Vaga("validNumeroVaga", true, 1L, validMorador);
+		validVaga.setId(validId);
+		Optional<Vaga> expectedVaga = Optional.of(validVaga);
+		VagaDto validVagaDto = new VagaDto(
+				expectedVaga.get().getNumeroDaVaga(),
+				expectedVaga.get().getEhAlugada(),
+				expectedVaga.get().getMoradorQueAlugou(),
+				expectedVaga.get().getMorador().getId());
+		
+		when(service.getVagaById(validId)).thenReturn(expectedVaga.get());
+		
+		ResponseEntity<VagaDto> response = controller.getVagaById(validId);
+		
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(validVagaDto, response.getBody());	
+	}
+	
+	@Test
+	public void testFindVagaByIdDeveRetornarHttp404() {
+		
+		Long invalidId = 99L;
+		
+		doThrow(NotFoundObjectException.class).when(service).getVagaById(invalidId);
+		
+		assertThrows(NotFoundObjectException.class, () -> controller.getVagaById(invalidId));
+	}
+	
+	@Test
+	public void testUpdateVagaByIdDeveRetornarHttp204() {
+		
+		Morador validMorador = new Morador("validNomedoMorador", "validApartamento", "validBloco");
+		validMorador.setId(2L);
+		Long validId = 1L;
+		VagaDto uptVagaDto = new VagaDto("uptValidNumeroVaga", true, 1L, 2L);
+		Vaga updatedVaga = new Vaga("updatedNumeroVaga", true, 1L, validMorador);
+		VagaDto expectedVagaDto = new VagaDto(
+				updatedVaga.getNumeroDaVaga(),
+				updatedVaga.getEhAlugada(),
+				updatedVaga.getMoradorQueAlugou(),
+				updatedVaga.getMorador().getId());
+		
+		when(service.updateVagaById(validId, uptVagaDto)).thenReturn(updatedVaga);
+		
+		ResponseEntity<VagaDto> response = controller.updateVagaById(validId, uptVagaDto);
+		
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+		assertEquals(expectedVagaDto, response.getBody());		
+	}
+	
+	@Test
+	public void testUpdateVagaByIdDeveRetornarHttp404() {
+		
+		Long invalidId = 99L;
+		VagaDto uptVagaDto = new VagaDto("uptValidNumeroVaga", true, 1L, 2L);
+		
+		doThrow(NotFoundObjectException.class).when(service).updateVagaById(invalidId, uptVagaDto);
+		
+		assertThrows(NotFoundObjectException.class, () -> controller.updateVagaById(invalidId, uptVagaDto));
+	}
+	
+	@Test
+	public void testDeleteVagaByIdDeveRetornarHttp204() {
+		
+		Long validId = 1L;
+		
+		doNothing().when(service).deleteVagaById(validId);
+		
+		ResponseEntity<Void> response = controller.deleteVagaById(validId);
+		
+		assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
 
+        verify(service).deleteVagaById(validId);
+	}
+	
+	@Test
+	public void testDeleteVagaByIdDeveRetornarHttp404() {
+		
+		Long invalidId = 99L;
+		
+		doThrow(NotFoundObjectException.class).when(service).deleteVagaById(invalidId);
+		
+		assertThrows(NotFoundObjectException.class, () -> controller.deleteVagaById(invalidId));
+	}
 }
--- src/test/java/com/example/teste/cond_vagas_garagem/controllers/VeiculoControllerTest.java
+++ src/test/java/com/example/teste/cond_vagas_garagem/controllers/VeiculoControllerTest.java
@@ -1,24 +1,157 @@
 package com.example.teste.cond_vagas_garagem.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.AfterEach;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.teste.cond_vagas_garagem.dtos.VeiculoDto;
+import com.example.teste.cond_vagas_garagem.exceptions.NotFoundObjectException;
+import com.example.teste.cond_vagas_garagem.exceptions.NotNullableFieldsException;
+import com.example.teste.cond_vagas_garagem.models.Morador;
+import com.example.teste.cond_vagas_garagem.models.Vaga;
+import com.example.teste.cond_vagas_garagem.models.Veiculo;
+import com.example.teste.cond_vagas_garagem.services.VeiculoService;
 
 class VeiculoControllerTest {
 
+	@Mock private VeiculoService service;
+	@InjectMocks VeiculoController controller;
+	
 	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testCadastrarVeiculoDeveRetornarHttpStatus201() {
+		
+		VeiculoDto validVeiculoDto = new VeiculoDto("validPlaca", "validModelo", "validCor", 1L, 1L);
+		VeiculoDto validVeiculoDtoSaved = new VeiculoDto("validPlaca", "validModelo", "validCor", 1L, 1L);
+		
+		when(service.saveVeiculo(validVeiculoDto)).thenReturn(validVeiculoDtoSaved);
+		
+		ResponseEntity<VeiculoDto> response = controller.addVeiculo(validVeiculoDto);
+		
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		assertEquals(validVeiculoDtoSaved, response.getBody());
+	}
+	
+	@Test
+	public void testCadastrarVeiculoDeveRetornarHttpStatus406() {
+		
+		VeiculoDto invalidVeiculoDto = null;
+		
+		doThrow(NotNullableFieldsException.class).when(service).saveVeiculo(invalidVeiculoDto);
+		
+		assertThrows(NotNullableFieldsException.class, () -> controller.addVeiculo(invalidVeiculoDto));
+	}
+	
+	@Test
+	public void testFindVeiculoByIdDeveRetornarHttp200() {
+	
+		Morador validMorador = new Morador("validNomedoMorador", "validApartamento", "validBloco");
+		Vaga validVaga = new Vaga("validNumeroVaga", true, 1L, validMorador);
+		Veiculo validVeiculo = new Veiculo("validPlaca", "validModelo", "validCor", validMorador, validVaga);
+		Long validId = 1L;
+		validVeiculo.setId(validId);
+		Optional<Veiculo> expectedVeiculo = Optional.of(validVeiculo);
+		VeiculoDto validVeiculoDto = new VeiculoDto(
+				expectedVeiculo.get().getPlacaDoVeiculo(),
+				expectedVeiculo.get().getModeloDoVeiculo(),
+				expectedVeiculo.get().getCorDoVeiculo(),
+				expectedVeiculo.get().getMorador().getId(),
+				expectedVeiculo.get().getVaga().getId());
+		
+		when(service.getVeiculoById(validId)).thenReturn(expectedVeiculo.get());
+		
+		ResponseEntity<VeiculoDto> response = controller.getVeiculoById(validId);
+		
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(validVeiculoDto, response.getBody());
 	}
 
+	@Test
+	public void testFindVeiculoByIdDeveRetornarHttp404() {
+	
+		Long invalidId = 99L;
+		
+		doThrow(NotFoundObjectException.class).when(service).getVeiculoById(invalidId);
+		
+		assertThrows(NotFoundObjectException.class, () -> controller.getVeiculoById(invalidId));
+	}
+	
+	@Test
+	public void testUpdateVeiculoByIdDeveRetornarHttp204() {
+	
+		Morador validMorador = new Morador("validNomedoMorador", "validApartamento", "validBloco");
+		Vaga validVaga = new Vaga("validNumeroVaga", true, 1L, validMorador);
+		Long validId = 1L;
+		VeiculoDto uptVeiculoDto = new VeiculoDto("uptPlaca", "uptModelo", "uptCor", 1L, 1L);
+		Veiculo updatedVeiculo = new Veiculo("updatedPlaca", "updatedModelo", "updatedCor", validMorador, validVaga);
+		VeiculoDto expectedVeiculo = new VeiculoDto(
+				updatedVeiculo.getPlacaDoVeiculo(),
+				updatedVeiculo.getModeloDoVeiculo(),
+				updatedVeiculo.getCorDoVeiculo(),
+				updatedVeiculo.getMorador().getId(),
+				updatedVeiculo.getVaga().getId());
+				
+		when(service.updateVeiculoById(validId, uptVeiculoDto)).thenReturn(updatedVeiculo);
+		
+		ResponseEntity<VeiculoDto> response = controller.updateVeiculoById(validId, uptVeiculoDto);
+		
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+		assertEquals(expectedVeiculo, response.getBody());
+	}
+	
+	@Test
+	public void testUpdateVeiculoByIdDeveRetornarHttp404() {
+	
+		Long invalidId = 99L;
+		VeiculoDto uptVeiculoDto = new VeiculoDto("uptPlaca", "uptModelo", "uptCor", 1L, 1L);
+		
+		doThrow(NotFoundObjectException.class).when(service).updateVeiculoById(invalidId, uptVeiculoDto);
+		
+		assertThrows(NotFoundObjectException.class, () -> controller.updateVeiculoById(invalidId, uptVeiculoDto));
+	}
+	
+	@Test
+	public void testDeleteVeiculoByIdDeveRetornarHttp204() {
+	
+		Long validId = 1L;
+		
+		doNothing().when(service).deleteVeiculoById(validId);
+		
+		ResponseEntity<Void> response = controller.deleteVeiculoById(validId);
+		
+		assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+		
+		verify(service).deleteVeiculoById(validId);
+	}
+	
+	@Test
+	public void testDeleteVeiculoByIdDeveRetornarHttp404() {
+	
+		Long invalidId = 99L;
+		
+		doThrow(NotFoundObjectException.class).when(service).deleteVeiculoById(invalidId);
+		
+		assertThrows(NotFoundObjectException.class, () -> controller.deleteVeiculoById(invalidId));		
+	}
 }
